e25a5e0b5f3f2f4862c0113961fdec7259ab87eb
Nicholas Mason <nmt.mason@googlemail.com>
2018-06-10 13:03:37 +0100

    Run index.js through prettier

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ var express = require('express');
 
 var app = express();
 
-app.get('/', function (request, response) {
+app.get('/', function(request, response) {
   response.send({
-    ipaddress: request.headers['x-forwarded-for'] || request.connection.remoteAddress,
+    ipaddress:
+      request.headers['x-forwarded-for'] || request.connection.remoteAddress,
     language: request.headers['accept-language'].split(',')[0],
     software: /\((.*?)\)/.exec(request.headers['user-agent'])[1]
   });
